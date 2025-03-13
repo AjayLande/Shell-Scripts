acf77c06009743cd4a7ca1217cf163a908a39793
Ajay Lande <36320661+AjayLande@users.noreply.github.com>
2025-03-13 18:49:12 +0530

    Update finddir.sh

--- finddir.sh
+++ finddir.sh
@@ -1,19 +1,23 @@
-# This script will get the input from customer and check if the directory exist or not , if directory not exist then it will create it.
+# This script will get the input from the customer and check if the directory exists or not.
+# If the directory does not exist, it will prompt the user to create a new directory.
 
-echo " Enter the full path of directory "
-read fdir
-if [ !-d fdir ]; then
-  echo " Directory Exist"
+echo " Enter the full path of directory "  # Prompt user to enter the directory path
+read fdir  # Read user input
+
+# Check if the directory exists
+if [ ! -d fdir ]; then
+  echo " Directory Exist"  # Print message if directory exists
 else 
-  echo " Directory Not Exist"
+  echo " Directory Not Exist"  # Print message if directory does not exist
   echo " "
+
   echo " Enter the full path of Parent directory where you want to create new directory"
-  read dpath;
-  cd $dpath;
+  read dpath  # Read the parent directory path
+  cd $dpath  # Navigate to the parent directory
+
   echo " Enter the Name of directory you want to create "
-  read newdirname;
-  mkdir $newdirname;
-  echo " New Directory $newdirname created under $dpath"
+  read newdirname  # Read the new directory name
+  mkdir $newdirname  # Create the new directory
+  echo " New Directory $newdirname created under $dpath"  # Confirmation message
 
 fi
-
